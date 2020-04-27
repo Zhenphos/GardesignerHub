4a247b0e7d4cc0378073530cc53dd9125f1e7e4b
Haseebullah Siddiqui <haseebs@udel.edu>
2020-04-27 13:03:12 -0400

    added main menu button to plant placement screen

--- src/view/PlantPlacementScene.java
+++ src/view/PlantPlacementScene.java
@@ -27,61 +27,58 @@ import mvc.View;
  */
 
 public class PlantPlacementScene extends Scene {
-	static Group root = new Group();
-	
+	static Group plantPlacementGroup = new Group();
+
 	public PlantPlacementScene() {
-		super(root);
+		super(plantPlacementGroup);
 		placePlant();
 	}
-	
+
 	/**
-	 * Creates the plant placement scene which allows the user to drag
-	 * and drop plants onto the garden space they drew previously.
+	 * Creates the plant placement scene which allows the user to drag and drop
+	 * plants onto the garden space they drew previously.
 	 */
 	public void placePlant() {
 		Canvas drawCanvas = new Canvas(View.getCanvasWidth(), View.getCanvasHeight());
 		GraphicsContext drawGC;
-		root.getChildren().add(drawCanvas);
+		plantPlacementGroup.getChildren().add(drawCanvas);
 		drawGC = drawCanvas.getGraphicsContext2D();
 		drawGC.clearRect(0, 0, View.getCanvasWidth(), View.getCanvasHeight());
-		
-		
+
 		BorderPane border = new BorderPane();
 		VBox top = new VBox(5);
 		HBox imageBar = new HBox(10);
-		root.getChildren().add(border);
+		plantPlacementGroup.getChildren().add(border);
 		AnchorPane center = new AnchorPane();
-		BorderPane.setMargin(top, new Insets( 50,12.5,50,12.5));
+		BorderPane.setMargin(top, new Insets(50, 12.5, 50, 12.5));
 
 		border.setTop(top);
 		border.setCenter(center);
 		Text scenetitle = new Text("Drag and Drop Plants");
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		scenetitle.setTextAlignment(TextAlignment.CENTER);
-		top.getChildren().add(scenetitle); 
+		top.getChildren().add(scenetitle);
 		top.setAlignment(Pos.CENTER);
 		imageBar.minHeight(100);
 		imageBar.minWidth(View.getCanvasWidth());
 		imageBar.maxHeight(100);
 		imageBar.setStyle("-fx-border-color: black");
-		imageBar.setPadding(new Insets(50,0,50,0));
-		for(int i=0; i<10; i++) {
-			imageBar.getChildren().add(new Label("{Plant image " + (int)(i+1)+"}"));
+		imageBar.setPadding(new Insets(50, 0, 50, 0));
+		for (int i = 0; i < 10; i++) {
+			imageBar.getChildren().add(new Label("{Plant image " + (int) (i + 1) + "}"));
 			imageBar.getChildren().add(new Separator(Orientation.VERTICAL));
 		}
 		top.getChildren().add(imageBar);
 
-		
-		BorderPane.setMargin(center, new Insets(0,12.5,0,12.5));
-		center.setPadding(new Insets(View.getCanvasHeight()/2.5,50, 0, 0));
+		BorderPane.setMargin(center, new Insets(0, 12.5, 0, 12.5));
+		center.setPadding(new Insets(View.getCanvasHeight() / 2.5, 50, 0, 0));
 		BackgroundFill background_fill = new BackgroundFill(Color.FORESTGREEN, CornerRadii.EMPTY, Insets.EMPTY);
-        Background background = new Background(background_fill);
-        center.setBackground(background);
+		Background background = new Background(background_fill);
+		center.setBackground(background);
 		center.setStyle("-fx-border-color: black");
-		
-		
+
 		Button prevButton = new Button("Prev");
-		root.getChildren().add(prevButton);
+		plantPlacementGroup.getChildren().add(prevButton);
 		prevButton.setTranslateX(View.getCanvasWidth() / 2 - View.getCanvasWidth() / 4);
 		prevButton.setTranslateY(600);
 
@@ -92,9 +89,9 @@ public class PlantPlacementScene extends Scene {
 		};
 
 		prevButton.setOnAction(prevButtonAction);
-		
+
 		Button nextButton = new Button("Next");
-		root.getChildren().add(nextButton);
+		plantPlacementGroup.getChildren().add(nextButton);
 		nextButton.setTranslateX(View.getCanvasWidth() / 2 + View.getCanvasWidth() / 4);
 		nextButton.setTranslateY(600);
 
@@ -108,7 +105,7 @@ public class PlantPlacementScene extends Scene {
 
 		// TODO replace with selecting plant from image
 		Button plantButton = new Button("Choose a Plant");
-		root.getChildren().add(plantButton);
+		plantPlacementGroup.getChildren().add(plantButton);
 		plantButton.setTranslateX(View.getCanvasWidth() / 2);
 		plantButton.setTranslateY(210);
 
@@ -119,7 +116,24 @@ public class PlantPlacementScene extends Scene {
 		};
 
 		plantButton.setOnAction(plantButtonAction);
-		
+
+		// main menu button start
+
+		Button mainMenuButton = new Button("Main Menu");
+		plantPlacementGroup.getChildren().add(mainMenuButton);
+		mainMenuButton.setTranslateX(View.getCanvasWidth() / 2 - 20);
+		mainMenuButton.setTranslateY(600);
+
+		EventHandler<ActionEvent> mainMenuButtonAction = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				View.getStage().setScene(View.getMainMenuScene());
+			}
+		};
+
+		mainMenuButton.setOnAction(mainMenuButtonAction);
+
+		// main menu button end
+
 	}
 
 }
