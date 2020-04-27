121d91e97058c4c0f13ba3a740e7191a13bab622
Jason <jasonhe@udel.edu>
2020-04-27 02:17:33 -0400

    Changed next button to new

--- src/view/MainMenuScene.java
+++ src/view/MainMenuScene.java
@@ -41,6 +41,17 @@ public class MainMenuScene extends Scene {
 		mainMenuGroup.getChildren().add(newButton);
 		newButton.setTranslateX(View.getCanvasWidth() / 2 - View.getCanvasWidth() / 4);
 		newButton.setTranslateY(500);
+		
+		// "New" Button event handler
+		EventHandler<ActionEvent> newButtonAction = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				System.out.println("new button triggered");
+				View.getStage().setScene(View.getGardenInfoScene());
+			}
+		};
+
+		// "New" Button on click action
+		newButton.setOnAction(newButtonAction);
 
 		// "Import" button
 		Button importButton = new Button("Import");
@@ -63,24 +74,6 @@ public class MainMenuScene extends Scene {
 		// TODO below are test buttons for screen flipping
 		// they can probably be removed or modified when
 		// the other buttons are implemented
-
-		// "next screen" button
-		Button nextButton = new Button("next");
-		mainMenuGroup.getChildren().add(nextButton);
-		nextButton.setTranslateX(View.getCanvasWidth() / 2 + View.getCanvasWidth() / 4);
-		nextButton.setTranslateY(600);
-
-		// nextButton event handler
-		EventHandler<ActionEvent> nextButtonAction = new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-				System.out.println("next button triggered");
-				View.getStage().setScene(View.getGardenInfoScene());
-			}
-		};
-
-		// nextButton on click action
-		nextButton.setOnAction(nextButtonAction);
-
 	}
 
 }
