87b6538188e57e22de8e18467ee0463cfe55c3e0
Haseebullah Siddiqui <haseebs@udel.edu>
2020-04-27 12:47:17 -0400

    add main menu button to drawgarden

--- src/view/DrawScene.java
+++ src/view/DrawScene.java
@@ -26,10 +26,10 @@ import mvc.View;
  */
 
 public class DrawScene extends Scene {
-	static Group drawGroup = new Group();
+	static Group drawGardenGroup = new Group();
 
 	public DrawScene() {
-		super(drawGroup);
+		super(drawGardenGroup);
 		createDraw();
 	}
 
@@ -39,55 +39,54 @@ public class DrawScene extends Scene {
 	public void createDraw() {
 		Canvas drawCanvas = new Canvas(View.getCanvasWidth(), View.getCanvasHeight());
 		GraphicsContext drawGC;
-		drawGroup.getChildren().add(drawCanvas);
+		drawGardenGroup.getChildren().add(drawCanvas);
 		drawGC = drawCanvas.getGraphicsContext2D();
 		drawGC.clearRect(0, 0, View.getCanvasWidth(), View.getCanvasHeight());
-		
+
 		BorderPane border = new BorderPane();
 		FlowPane left = new FlowPane();
 		HBox top = new HBox();
 		AnchorPane center = new AnchorPane();
-		drawGroup.getChildren().add(border);
+		drawGardenGroup.getChildren().add(border);
 		border.setTop(top);
 		border.setLeft(left);
 		border.setCenter(center);
-		
+
 		Text scenetitle = new Text("\t\t\t\tDraw Garden");
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		scenetitle.setTextAlignment(TextAlignment.CENTER);
-		top.getChildren().add(scenetitle); 
+		top.getChildren().add(scenetitle);
 		top.setAlignment(Pos.CENTER);
-		
 
-		Label plant = new Label("Plant \t {Plant Image}") ;
+		Label plant = new Label("Plant \t {Plant Image}");
 		Label road = new Label("Road \t {Road Image}");
 		Label stream = new Label("Stream \t {Stream Image}");
 		Label shade = new Label("Shade \t {Label Image}");
 		Label object = new Label("Object \t {Object Image}");
-		
-	    left.setPadding(new Insets(25, 25, 25, 25)); //(top, right. bottom, left)
-	    left.setStyle("-fx-border-color: black");
-	    left.setVgap(4);
-	    left.setHgap(4);
-	    left.setPrefWrapLength(150);
-	    BorderPane.setMargin(left, new Insets(100,25,25,25));
+
+		left.setPadding(new Insets(25, 25, 25, 25)); // (top, right. bottom, left)
+		left.setStyle("-fx-border-color: black");
+		left.setVgap(4);
+		left.setHgap(4);
+		left.setPrefWrapLength(150);
+		BorderPane.setMargin(left, new Insets(100, 25, 25, 25));
 		left.getChildren().addAll(plant, road, stream, shade, object);
-		
-		
-		center.setPadding(new Insets(150,450, 150, 150));
+
+		center.setPadding(new Insets(150, 450, 150, 150));
 		BackgroundFill background_fill = new BackgroundFill(Color.FORESTGREEN, CornerRadii.EMPTY, Insets.EMPTY);
-        Background background = new Background(background_fill);
-        center.setBackground(background);
+		Background background = new Background(background_fill);
+		center.setBackground(background);
 		center.setStyle("-fx-border-color: black");
 
-	    BorderPane.setMargin(center, new Insets(100,0,0,100));
+		BorderPane.setMargin(center, new Insets(100, 0, 0, 100));
 
 		Image drawBackground;
 		drawBackground = View.createImage("resources/drawImage.png");
-		//drawGC.drawImage(drawBackground, 0, 0, View.getCanvasWidth(), View.getCanvasHeight());
-		
+		// drawGC.drawImage(drawBackground, 0, 0, View.getCanvasWidth(),
+		// View.getCanvasHeight());
+
 		Button prevButton = new Button("prev");
-		drawGroup.getChildren().add(prevButton);
+		drawGardenGroup.getChildren().add(prevButton);
 		prevButton.setTranslateX(View.getCanvasWidth() / 2 - View.getCanvasWidth() / 4);
 		prevButton.setTranslateY(600);
 
@@ -98,9 +97,9 @@ public class DrawScene extends Scene {
 		};
 
 		prevButton.setOnAction(prevButtonAction);
-		
+
 		Button nextButton = new Button("next");
-		drawGroup.getChildren().add(nextButton);
+		drawGardenGroup.getChildren().add(nextButton);
 		nextButton.setTranslateX(View.getCanvasWidth() / 2 + View.getCanvasWidth() / 4);
 		nextButton.setTranslateY(600);
 
@@ -111,5 +110,22 @@ public class DrawScene extends Scene {
 		};
 
 		nextButton.setOnAction(nextButtonAction);
+
+		// main menu button start
+
+		Button mainMenuButton = new Button("Main Menu");
+		drawGardenGroup.getChildren().add(mainMenuButton);
+		mainMenuButton.setTranslateX(View.getCanvasWidth() / 2 - 20);
+		mainMenuButton.setTranslateY(600);
+
+		EventHandler<ActionEvent> mainMenuButtonAction = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				View.getStage().setScene(View.getMainMenuScene());
+			}
+		};
+
+		mainMenuButton.setOnAction(mainMenuButtonAction);
+
+		// main menu button end
 	}
 }
