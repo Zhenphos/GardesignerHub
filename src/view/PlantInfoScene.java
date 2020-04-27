a485eaaeb52fb109ec672e36d7bfe638e456ecb2
Jonathan Zhang <Jonathan.Zhang111@gmail.com>
2020-04-27 09:42:38 -0400

    Added PlantInfoScene

--- src/view/PlantInfoScene.java
+++ src/view/PlantInfoScene.java
@@ -3,17 +3,22 @@ package view;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -53,24 +58,44 @@ public class PlantInfoScene extends Scene {
 		Image plantImage;
 		// TODO replace with variable that gets image corresponding to selected plant
 		plantImage = View.createImage("resources/commonMilkweed.png");
-		plantInfoGC.drawImage(plantImage, 0, View.getCanvasHeight() / 6, View.getCanvasWidth() / 2, View.getCanvasHeight() / 2);
+		plantInfoGC.drawImage(plantImage, -20, View.getCanvasHeight() / 5.6, View.getCanvasWidth() / 2, View.getCanvasHeight() / 2);
 		
 		BorderPane border = new BorderPane();
-		HBox top = new HBox();
 		FlowPane left = new FlowPane();
-		AnchorPane center = new AnchorPane();
+		HBox center = new HBox();
+		VBox right = new VBox();
 		
 		plantInfoGroup.getChildren().add(border);
-		border.setTop(top);
 		border.setLeft(left);
 		border.setCenter(center);
+		border.setRight(right);
 		
 		// TODO replace with file of plant names and change to take corresponding plant name from file
-		Text sceneTitle = new Text("\nPlant Name");
+		Text sceneTitle = new Text("\n\tPlant Name");
 		sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		sceneTitle.setTextAlignment(TextAlignment.CENTER);
-		top.getChildren().add(sceneTitle);
-		top.setAlignment(Pos.CENTER);
+		center.getChildren().add(sceneTitle);
+		center.setAlignment(Pos.TOP_CENTER);
+		
+		Label light = new Label("Light info") ;
+		Label water = new Label("Water info");
+		Label bloomTime = new Label("Bloom time info");
+		Label food = new Label("Food info");
+		Label bloomColor = new Label("Bloom color info");
+		Label soilReqs = new Label("Soil requirements info");
+		
+	    right.minHeight(View.getCanvasHeight());
+	    right.minWidth(View.getCanvasWidth() / 2);
+	    right.maxHeight(View.getCanvasHeight());
+	    right.setPadding(new Insets(100, 200, 100, 50));
+	    right.setStyle("-fx-border-color: black");
+		right.getChildren().addAll(light, new Label(""), 
+				water, new Label(""), 
+				bloomTime, new Label(""), 
+				food, new Label(""), 
+				bloomColor, new Label(""), 
+				soilReqs);
+		BorderPane.setMargin(right, new Insets(View.getCanvasHeight() / 7, 50, 0, 0));
 		
 		Button prevButton = new Button("prev");
 		plantInfoGroup.getChildren().add(prevButton);
