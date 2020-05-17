34dad0e933f82b3d0d8d7c03b7aeee6cb7ddef2a
Haseeb <haseebs@udel.edu>
2020-05-16 20:27:23 -0400

    add plant list 17

--- scraper/NewMoonNursery/New Moon Nursery Scraper.py
+++ scraper/NewMoonNursery/New Moon Nursery Scraper.py
@@ -71,12 +71,12 @@ csvFields = ['Botanical Name', 'Height',
              'Extra Attributes', 'Deer Resistant', 'Foliage Color', 'Growth Rate', 'Salt Tolerance',
              'Season of Interest', 'Phytoremediation']
 csvRows = []
-csvFileName = 'C:/Users/ts140/eclipse-workspace-java-hp/team-11-2/scraper/NewMoonNursery/Results/PlantList_18.csv'
+csvFileName = 'C:/Users/ts140/eclipse-workspace-java-hp/team-11-2/scraper/NewMoonNursery/Results/PlantList_17.csv'
 basicAttributeSoup = []
 oneString = ""
 
 # change this for whatever plant list you want to get data for
-pathToPlantList = PlantList_18
+pathToPlantList = PlantList_17
 
 urlBase = 'http://www.newmoonnursery.com/'
 
